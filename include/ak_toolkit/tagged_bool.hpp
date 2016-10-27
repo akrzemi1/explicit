a3a74b6f3068ee14a601772ddaf79859f2c8a91b
Andrzej Krzemieński <akrzemi1@gmail.com>
2016-10-27 15:36:03 +0200

    Create tagged_bool.hpp

---
+++ include/ak_toolkit/tagged_bool.hpp
@@ -0,0 +1,38 @@
+// Copyright (C) 2016 Andrzej Krzemienski.
+//
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef AK_TOOLKIT_TAGGED_BOOL_HEADER_GUARD_
+#define AK_TOOLKIT_TAGGED_BOOL_HEADER_GUARD_
+
+namespace ak_toolkit {
+namespace tagged_bool_ns {
+
+template <typename Tag>
+class tagged_bool
+{
+    bool value;
+    
+public:
+  
+    constexpr explicit tagged_bool (bool v) : value {v} {}
+    
+    constexpr explicit tagged_bool (int) = delete;
+    constexpr explicit tagged_bool (double) = delete;
+    constexpr explicit tagged_bool (void*) = delete;
+    
+    constexpr explicit operator bool() const { return value; }
+    
+    friend constexpr bool operator==(tagged_bool l, tagged_bool r) { return l.value == r.value; }
+    friend constexpr bool operator!=(tagged_bool l, tagged_bool r) { return l.value != r.value; }
+};
+
+}
+
+using tagged_bool_ns::tagged_bool;
+
+}
+
+#endif
