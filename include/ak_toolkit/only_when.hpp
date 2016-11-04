dbf6d04b419c509d867e3d993ed25dbc0cd5fca4
Andrzej Krzemieński <akrzemi1@gmail.com>
2016-11-04 19:49:09 +0100

    Update only_when.hpp

--- include/ak_toolkit/only_when.hpp
+++ include/ak_toolkit/only_when.hpp
@@ -27,12 +27,25 @@ public:
 
 #undef AK_TOOLKIT_ENABLE_IF
 
+
+# if (defined __GNUC__) && (!defined __clang__) && (__GNUC__ < 4 || __GNUC__ == 4 && __GNUC_MINOR__ <= 7)
+
+template <typename T>
+struct is_signed_integral : ::std::conditional<
+  ::std::is_signed<T>::value && ::std::is_integral<T>::value,
+  ::std::true_type,
+  ::std::false_type>::type {};  
+  
+# else
+  
 template <typename T>
 using is_signed_integral = typename ::std::conditional<
   ::std::is_signed<T>::value && ::std::is_integral<T>::value,
   ::std::true_type,
   ::std::false_type>::type;
 
+# endif
+  
 }
 
 using only_when_ns::only_when;           // this prevents inadvertent ADL
@@ -40,8 +53,16 @@ using only_when_ns::is_signed_integral;  //
 
 using only_int = only_when<int, is_signed_integral>;
 
+# if (defined __GNUC__) && (!defined __clang__) && (__GNUC__ < 4 || __GNUC__ == 4 && __GNUC_MINOR__ <= 7)
+
+  // sorry not supported
+  
+# else
+  
 template <typename T>
   using only_lvalue = only_when<T&, ::std::is_lvalue_reference>;
+
+# endif
 
 }
 }
