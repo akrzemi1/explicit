d0532a46951efa8699a3028bc4595c35ccfa251d
Andrzej Krzemieński <akrzemi1@gmail.com>
2016-11-11 01:24:02 +0100

    better only_int

--- include/ak_toolkit/only_when.hpp
+++ include/ak_toolkit/only_when.hpp
@@ -46,12 +46,44 @@ using is_signed_integral = typename ::std::conditional<
 
 # endif
   
+namespace detail {
+
+template <typename I, typename T>
+std::false_type test_no_narrowing(long long);
+
+template <typename I, typename T>
+auto test_no_narrowing(int) -> decltype(I{::std::declval<T>()}, ::std::true_type{}) ;
+
+template <typename I>
+struct int_no_double_test
+{
+  int_no_double_test(I) {}
+  int_no_double_test(long double) = delete;
+  int_no_double_test(unsigned long long) = delete;
+};
+  
+} // namespace detail
+  
+#if defined __GNUC__ && ! defined __clang__
+template <typename I, typename T>
+struct is_int_convertible_but_no_float : ::std::conditional<decltype(detail::test_no_narrowing<I, T>(1))::value 
+                                                            && ::std::is_convertible<T, I>::value
+                                                            && std::is_constructible<detail::int_no_double_test<I>, T>::value,
+  std::true_type, std::false_type>::type {}  ;
+#else
+template <typename I, typename T>
+struct is_int_convertible_but_no_float : ::std::conditional<decltype(detail::test_no_narrowing<I, T>(1))::value
+                                                            && ::std::is_convertible<T, I>::value,
+  std::true_type, std::false_type>::type {}  ;
+#endif
+  
 }
 
-using only_when_ns::only_when;           // this prevents inadvertent ADL
-using only_when_ns::is_signed_integral;  //
+using only_when_ns::only_when;                       // this prevents inadvertent ADL
+using only_when_ns::is_signed_integral;              //
+using only_when_ns::is_int_convertible_but_no_float; //
 
-typedef only_when<int, is_signed_integral> only_int;
+typedef only_when<int, is_int_convertible_but_no_float> only_int;
 
 # if (defined __GNUC__) && (!defined __clang__) && (__GNUC__ < 4 || __GNUC__ == 4 && __GNUC_MINOR__ <= 7)
 
